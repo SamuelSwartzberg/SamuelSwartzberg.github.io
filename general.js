7ce77d7d82ccfb2e596f62f397630e2ff4be8915
SamuelSwartzberg <samuel.swartzberg@outlook.com>
2020-03-18 16:43:11 +0100

    New layout ctd.

--- general.js
+++ general.js
@@ -3,4 +3,50 @@ document.querySelector(".code-selector").onchange = function (e,i) {
         item.style.display="none";
     });
     document.querySelector("#code .mockup-body ."+e.target.value).style.display="block";
-}
+};
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('pre code').forEach((block) => {
+    hljs.highlightBlock(block);
+    console.log("hey");
+  });
+  document.querySelector("#name-input").oninput = function (e,i){
+    if(e.target.value.match(/[\{\}\[\]\(\)\;\=\+\-\"\*\:\>\<\!\,\.\-\_\@\%\&\?\^]/g)){
+      alert("Due to security concerns, you may not enter special characters.\nPlease try again.");
+      e.target.value = "";
+    } else{
+      document.querySelector("#intro h1 .insert-name").innerText=" "+e.target.value;
+    }
+    console.log(e.target.value);
+  };
+  var themeChanger=document.querySelector(".theme-changer");
+  themeChanger.onclick=function(e, i){
+    document.querySelector("body").classList.toggle("dark-theme");}
+  var images = document.querySelectorAll(".image-container img");
+  for (var i = 0; i < images.length; i++) {
+    var bubble = document.createElement('div');
+    bubble.setAttribute('class','img-bubble');
+    if(i==0){bubble.setAttribute('class','img-bubble active');}
+    document.querySelector(".image-bubbles").appendChild(bubble);
+  }
+  function switchImage(plusMinus){
+    var activeImage = document.querySelector(".image-container img.active");
+    var currentIndex = Array.from(activeImage.parentNode.children).indexOf(activeImage);
+    var currentIndexCremented = currentIndex+plusMinus;
+    var imageBubbles = document.querySelector(".image-bubbles")
+    var imageContainer = document.querySelector(".image-container");
+    if (currentIndexCremented>=0 && currentIndexCremented<imageContainer.children.length){
+      activeImage.classList.remove("active");
+      imageContainer.children[currentIndexCremented].classList.add("active");
+      imageBubbles.children[currentIndex].classList.remove("active");
+      imageBubbles.children[currentIndexCremented].classList.add("active");
+    }
+  }
+
+  document.querySelector(".clickable-l").onclick = function () {
+    switchImage(-1);
+  };
+  document.querySelector(".clickable-r").onclick = function () {
+    switchImage(1);
+  };
+
+});
